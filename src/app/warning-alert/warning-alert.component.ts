60e90fa1e19288506b2654106b2211f37b2687f0
h_dinari <h_dinari@modernisc.com>
2020-12-21 12:01:17 +0330

    second

--- src/app/warning-alert/warning-alert.component.ts
+++ src/app/warning-alert/warning-alert.component.ts
@@ -1,12 +1,21 @@
 import {Component} from '@angular/core';
+
 @Component({
   selector: 'app-warning-alert',
   template:
   `
      <p>This is a warning, you are in a danger!</p>
-  `
+  `,
+  styles: [
+      `
+      p {
+        padding: 1px;
+        background-color: mistyrose;
+        border: 2px solid red;
+      }
+    `
+  ]
 })
 
 export class WarningAlertComponent {
-  
 }
